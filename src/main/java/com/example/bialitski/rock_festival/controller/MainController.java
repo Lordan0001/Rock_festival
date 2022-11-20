7c253f1dfccc001bb75982fb9fe7b8029745719b
Vladislav <70939188+Lordan0001@users.noreply.github.com>
2022-11-20 09:31:16 +0300

    delete temp table + add mapping

--- src/main/java/com/example/bialitski/rock_festival/controller/MainController.java
+++ src/main/java/com/example/bialitski/rock_festival/controller/MainController.java
@@ -1,11 +1,8 @@
 package com.example.bialitski.rock_festival.controller;
 
 import com.example.bialitski.rock_festival.interfaces.AlbumsRepository;
-import com.example.bialitski.rock_festival.interfaces.CustomerRepository;
 import com.example.bialitski.rock_festival.interfaces.GroupsRepository;
 import com.example.bialitski.rock_festival.model.Albums;
-import com.example.bialitski.rock_festival.model.Artist;
-import com.example.bialitski.rock_festival.model.Customer;
 import com.example.bialitski.rock_festival.model.Groups;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,18 +12,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import java.util.ArrayList;
+
 import java.util.List;
 @Controller
 public class MainController {
-public static List<Artist> artists = new ArrayList<Artist>();
-static {
-    artists.add(new Artist("Marilyn Manson","We Are Chaos"));
-    artists.add(new Artist("Rob Zombie","The Lunar Injection Kool Aid Eclipse Conspiracy"));
-}
-
-    @Autowired
-    private CustomerRepository customerRepo;
 
     @Autowired
     private AlbumsRepository albumsRepo;
@@ -50,42 +39,28 @@ static {
         return modelAndView;
     }
 
-    //all artists
-    @RequestMapping(value = {"/artistList"}, method = RequestMethod.GET)
-    public ModelAndView artistList(Model model) {
+    //all groups
+    @RequestMapping(value = {"/GroupList"}, method = RequestMethod.GET)
+    public ModelAndView groupList(Model model) {
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("artistList");
-        model.addAttribute("music", artists);
+        modelAndView.setViewName("GroupList");
+        List<Groups> listGroups = groupsRepo.findAll();
+        model.addAttribute("listGroups", listGroups);
 
-        model.addAttribute("grList", grList);
+        model.addAttribute("grList", grList);//message
         return modelAndView;
     }
 
-//ща буит не темп
-//Temp
-@GetMapping("/customers")
-public String listAll(Model model) {
-    List<Customer> listCustomers = customerRepo.findAll();
-    model.addAttribute("listCustomers", listCustomers);
 
-    return "customers";
-}
 
-    @GetMapping("/albums")
+    @GetMapping("/AlbumsList")
     public String listAllAlbums(Model model) {
         List<Albums> listAlbums = albumsRepo.findAll();
         model.addAttribute("listAlbums", listAlbums);
 
-        return "FrontAlbums";
+        return "AlbumsList";
     }
 
-    @GetMapping("/groups")
-    public String listAllGroups(Model model) {
-        List<Groups> listGroups = groupsRepo.findAll();
-        model.addAttribute("listGroups", listGroups);
-
-        return "FrontGroups";
-    }
 
 
 
